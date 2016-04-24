fa81cef87219b529bae74e9ea7ddd0e39f16ed1c
pkohn <phil@seekpanda.com>
2016-04-23 22:11:57 -0400

    project 2 (almost done)

--- main.js
+++ main.js
@@ -13,52 +13,122 @@
 
 console.log("Question 1");
 
+
+var emptyarray = [ ];
+emptyarray.splice(0, 0, 1, 2, 3, 4);
+
+console.log(emptyarray);
+
 // 2. Add an additional item to the beginning of your array.
+
+console.log("Question 2");
+emptyarray.splice(0, 0, 0);
+console.log(emptyarray);
 
 // 3. Remove the second and third items in your array.
 
+console.log("Question 3");
+emptyarray.splice(1, 2);
+console.log(emptyarray);
+
 // 4. Add two new items after the second item.
 
+console.log("Question 4");
+emptyarray.splice(2, 0, 3.25, 3.5);
+console.log(emptyarray);
+
 // 5. Write 'The current length of the array is....' using the .length method
+
+console.log("Question 5");
+console.log ("The current length of the array is " + emptyarray.length);
 
 // Use the following Array for questions 6-11:
 var things = ['mug', 'book', 'mouse', 'plant', 'sunglasses'];
 
+
 // 6. Change 'mouse' to 'keyboard'
+
+console.log("Question 6");
+things[2] = 'keyboard';
+console.log(things);
 
 // 7. Combine all of the elements of the array into a string.
 //    (Hint: check out the 'join' method.)
 
+console.log("Question 7");
+
+var wildthings = things.join();
+console.log(wildthings);
+
 // 8. Declare a variable called lastItem using .pop() on the things array.
 //    Add two new items to lastItem, one at the beginning and one at the end.
+
+console.log("Question 8");
+
+var lastItem = things.pop();
+//lastItem.unshift('chopsticks');
+//lastItem.push('Soy');
+console.log(lastItem);
+
 
 // 9. Create a new Array called itemLast.
 //    The items should be the same as lastItem, only in reverse order.
 
+console.log("Question 9");
+
+var itemLast = lastItem.slice();
+lastItem.reverse;
+console.log(lastItem);
+ 
+
 // 10. Remove the first item of itemLast.
+
+console.log("Question 10");
+
+//var freeitemLast = shift.itemLast;
+console.log(itemLast);
 
 // 11. Remove all items from itemLast.
 
+console.log("Question 11");
+
 // 12. Using the Arrays below, create a single Array
 //     called numberPets whose value is [12, 5, 9, 27, 'fish', 'dog']
+
+console.log("Question 12");
 var firstArray = [12, 5, 9, 27];
 var secondArray = ['fish', 'dog'];
+
+var numberPets = firstArray.slice(); 
+//numberPets.splice(2, 0, secondArray);
+console.log(numberPets);
+
 
 // Use the following array for questions 13-16:
 var people = ['Bill', 'Ted', 'Emily', 'Andrea', 'Doug'];
 
 // 13. Add two new people after 'Doug'
 
+console.log("Question 13");
+
 // 14. Remove everybody except 'Andrea' and 'Ted'
+
+console.log("Question 14");
 
 // 15. Add a new person to the beginning of the Array
 
+console.log("Question 15");
+
 // 16. Arrange the items alphabetically. Store this Array as orderedPeople
+
+console.log("Question 15");
 
 // 17. Create an array of arrays with the following three arrays:
 var array1 = ["Fido", "Spot", "Rex", "Sparky"]
 var array2 = ["Bulldog", "Lab", "Dalmation", "Beagle"]
 var array3 = ["White", "Black", "Spotted", "Tri-color"]
+
+console.log("Question 17");
 
 // Goal:
 var array4 = [
@@ -69,11 +139,17 @@ var array4 = [
 
 // 18. Remove "Sparky" and "White" from the above array of arrays.
 
+console.log("Question 18");
+
 // BONUS 1: Try to arrange the following items from smallest to largest:
 var sortingNumbers = [2, 5, 98, 55, 77, 300]
 // If it doesn't sort as expected, explain why.
+
+console.log("Bonus #1");
 
 // BONUS 2: Transform array1 into array2 using as few lines of code as you can without directly changing the value of an item (ie array1[0] = item)
 var array1 = [2, 'dog', 34, 'Bill', 'plant', 'mug', 17];
 // Goal:
 var array2 = ['plant', 17, 2, 'Bill', 'dog'];
+
+console.log("Bonus #2");
